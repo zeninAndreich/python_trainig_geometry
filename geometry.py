960d86ea66ed2af1b5aa3c47331fc3e4bf1eaf4e
zenin.a.s <zenin.a.s@effective-group.ru>
2023-04-26 13:02:40 +0300

    Тест 17 . 4 Коллекции и циклы. Урок 7 Элементы функционального программирования_map и filter.

--- geometry.py
+++ geometry.py
@@ -6,7 +6,7 @@ l = [Point(i,i*i) for i in range(-5, 6) ]
 l = list(map(lambda i : Point(i,i*i), range(-5, 6)))
 
 
-l2 = list(map(lambda p: Point(p.x, -p.y), l))
+l2 = list(filter(lambda p: p.x % 2 == 0, l))
 
 
 
