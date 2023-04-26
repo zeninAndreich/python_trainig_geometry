66a1b4c21074272aa0156b981ecf5f0643b19718
zenin.a.s <zenin.a.s@effective-group.ru>
2023-04-26 11:27:07 +0300

    Тест 11 . 4 Коллекции и циклы. Урок 5 Лямбда-выражения.

--- geometry.py
+++ geometry.py
@@ -1,15 +1,10 @@
 from geom2d import *
 from geom2d.point import Point
 
-l1 = [Point(2, 1), Point(0, 0), Point(1, 2)]
+l1 = [Point(3, 1), Point(0, 0), Point(1, 2)]
 
-def x(p):
-    return p.x
 
-def y(p):
-    return p.y
-
-l2 = sorted(l1, key= y)
+l2 = sorted(l1, key= lambda p: p.distance(Point(0, 0)))
 
 print(l1)
 print(l2)
